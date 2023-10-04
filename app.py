01c2a07541ccf429d4edacdd3015b95413bd448d
GrayWolfCode <jlee888624@gmail.com>
2023-10-04 20:58:23 +0900

    first

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials, storage
+import os
+import requests
+from moviepy.editor import *
+
+app = Flask(__name__)
+CORS(app)
+
+# Firebase configuration
+PROJECT_ID = os.environ.get('PROJECT_ID')
+PRIVATE_KEY_ID = os.environ.get('PRIVATE_KEY_ID')
+PRIVATE_KEY = os.environ.get('PRIVATE_KEY').replace('\\n', '\n')
+CLIENT_EMAIL = os.environ.get('CLIENT_EMAIL')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+AUTH_URI = os.environ.get('AUTH_URI')
+TOKEN_URI = os.environ.get('TOKEN_URI')
+AUTH_PROVIDER_CERT_URL = os.environ.get('AUTH_PROVIDER_CERT_URL')
+CLIENT_CERT_URL = os.environ.get('CLIENT_CERT_URL')
+ELEVEN_API_KEY=os.environ.get('ELEVEN_API_KEY')
+
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": PROJECT_ID,
+    "private_key_id": PRIVATE_KEY_ID,
+    "private_key": PRIVATE_KEY,
+    "client_email": CLIENT_EMAIL,
+    "client_id": CLIENT_ID,
+    "auth_uri": AUTH_URI,
+    "token_uri": TOKEN_URI,
+    "auth_provider_x509_cert_url": AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": CLIENT_CERT_URL
+})
+
+firebase_admin.initialize_app(
+    cred, {'storageBucket': 'storyboard-739ee.appspot.com'})
+
+def upload_to_firebase(file_name):
+    bucket = storage.bucket()
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(file_name)
+    blob.make_public()
+    return blob.public_url
+
+@app.route('/generate_video', methods=['POST'])
+def generate_video():
+    image_urls = request.json.get('image_urls').split('#')
+    image_descriptions = request.json.get('image_descriptions').split('#')
+    voice_id = request.json.get('voice_id')
+    
+    if len(image_urls) != len(image_descriptions):
+        return jsonify({"error": "The number of images does not match the number of descriptions."}), 400
+    
+    clips = []
+
+    for i, (img_url, description) in enumerate(zip(image_urls, image_descriptions)):
+        img_response = requests.get(img_url)
+        img_path = f"image_{i}.png"
+        with open(img_path, 'wb') as f:
+            f.write(img_response.content)
+
+        audio_url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+        headers = {
+            "Accept": "audio/mpeg",
+            "Content-Type": "application/json",
+            "xi-api-key": ELEVEN_API_KEY
+        }
+        data = {
+            "text": description,
+            "model_id": "eleven_monolingual_v1",
+            "voice_settings": {
+                "stability": 0.5,
+                "similarity_boost": 0.5
+            }
+        }
+        audio_response = requests.post(audio_url, json=data, headers=headers)
+        audio_path = f"audio_{i}.mp3"
+        with open(audio_path, 'wb') as f:
+            f.write(audio_response.content)
+
+        audio = AudioFileClip(audio_path)
+        img_clip = ImageClip(img_path, duration=audio.duration).set_audio(audio)
+        clips.append(img_clip)
+
+    final_video = concatenate_videoclips(clips, method="compose")
+    final_video_path = "output_video.mp4"
+    final_video.write_videofile(final_video_path, fps=24)
+
+    video_url = upload_to_firebase(final_video_path)
+    
+    return jsonify({"video_url": video_url})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
